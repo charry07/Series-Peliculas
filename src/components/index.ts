431972bfc37b0edfc835a2b010bcfaa934b67ae4
charry07 <charry072013@gmail.com>
2022-12-07 16:02:57 -0500

    App v1

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 
 export * from './GifList'
 export * from './SearchBar'
 export * from './Footer'
 export * from './AppBarMenu'
 export * from './MovieCard'
-=======
-
-export * from './GifList'
-export * from './SearchBar'
-export * from './Footer'
-export * from './AppBarMenu'
-export * from './ContentSite'
->>>>>>> bcabec6c9d55da95452b7c89e9bb98e748403d42
